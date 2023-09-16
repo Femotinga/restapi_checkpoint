49bdeb941ffe28584f81d74caf1b794ea42e2e43
Femotinga <femiadeliyi@gmail.com>
2023-09-16 16:11:08 +0100

    initial

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' });
+  }
+});
+
+// POST a new user
+router.post('/', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Error creating user' });
+  }
+});
+
+// PUT (edit) a user by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Error updating user' });
+  }
+});
+
+// DELETE a user by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Error deleting user' });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Routes
+app.use('/api/users', require('./routes/users'));
